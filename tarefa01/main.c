00a41af856ea831b98f1a2468163661bfa187d67
rianpuc <redlopes@sga.pucminas.br>
2026-08-15 21:40:29 -0300

    feat: tarefa 1 printing in order

---
+++ tarefa01/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main()
+{
+    int i;
+
+    #pragma omp parallel num_threads(2)
+    {
+        int tid = omp_get_thread_num();
+        #pragma omp for ordered schedule(dynamic)
+        for(i = 1; i <= 3; i++) 
+        {
+           #pragma omp ordered
+           printf("[PRINT1] T%d = %d \n",tid,i);
+           #pragma omp ordered
+           printf("[PRINT2] T%d = %d \n",tid,i);
+        }
+    }
+}
